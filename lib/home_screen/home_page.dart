9caf49be5ac961bec0dac2ef20f70c4360f3ff46
PAOLO BUFALINO MORA <21-e7110311@users.noreply.gitlab.tjrr.jus.br>
2021-02-01 19:02:02 -0400

    añade paginacion al home screen

--- lib/home_screen/home_page.dart
+++ lib/home_screen/home_page.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentPage;
+  PageController _pageController;
+
+  _HomePageState(){
+    _currentPage = 0;
+    _pageController = PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +22,24 @@ class HomePage extends StatelessWidget {
         actions: [
           IconButton(icon: Icon(Icons.search_sharp), onPressed: null),
           IconButton(icon: Icon(Icons.filter_list_sharp), onPressed: null)
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: pageChanged,
+        children: <Widget>[
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.purple,
+          ),
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -22,12 +53,26 @@ class HomePage extends StatelessWidget {
           BottomNavigationBarItem(icon: Icon(Icons.check_circle_outline_sharp), label: "Por Hacer"),
           BottomNavigationBarItem(icon: Icon(Icons.local_atm_sharp), label: "Recompenzas"),
         ],
-        currentIndex: 0,
+        currentIndex: _currentPage,
         unselectedItemColor: Colors.black54,
         selectedItemColor: Colors.blue,
+        onTap: pageChanger,
         showUnselectedLabels: true,
       ),
       drawer: Drawer(),
     );
+  }
+
+  pageChanged(int page){
+    setState((){
+      _currentPage = page;
+    });
+  }
+
+  pageChanger(int page){
+    setState((){
+      _pageController.animateToPage(page, duration: Duration(milliseconds: 300), curve: Curves.ease);
+      _currentPage = page;
+    });
   }
 }
