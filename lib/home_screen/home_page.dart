0544bb393d1c4b925e977fedf87b1d67e810bbaa
PAOLO BUFALINO MORA <21-e7110311@users.noreply.gitlab.tjrr.jus.br>
2021-02-09 23:44:37 -0400

    add example of stats widget

--- lib/home_screen/home_page.dart
+++ lib/home_screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gellohabits/home_screen/widgets/character_thumbnail.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,7 +10,7 @@ class _HomePageState extends State<HomePage> {
   int _currentPage;
   PageController _pageController;
 
-  _HomePageState(){
+  _HomePageState() {
     _currentPage = 0;
     _pageController = PageController();
   }
@@ -24,21 +25,54 @@ class _HomePageState extends State<HomePage> {
           IconButton(icon: Icon(Icons.filter_list_sharp), onPressed: null)
         ],
       ),
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: pageChanged,
-        children: <Widget>[
-          Container(
-            color: Colors.yellow,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 3,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox.fromSize(
+                      size: Size(300, 10),
+                      child: LinearProgressIndicator(
+                        value: 0.3,
+                        backgroundColor: Colors.grey,
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                      ),
+                    ),
+                    SizedBox.fromSize(
+                      size: Size(60, 60),
+                      child: CharacterThumbnail(color: Colors.red),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          Container(
-            color: Colors.purple,
-          ),
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
+          Expanded(
+            flex: 8,
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: pageChanged,
+              children: <Widget>[
+                Container(
+                  color: Colors.yellow,
+                ),
+                Container(
+                  color: Colors.purple,
+                ),
+                Container(
+                  color: Colors.red,
+                ),
+                Container(
+                  color: Colors.green,
+                ),
+              ],
+            ),
           ),
         ],
       ),
@@ -49,9 +83,12 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.alarm), label: "Habitos"),
-          BottomNavigationBarItem(icon: Icon(Icons.calendar_today_sharp), label: "Diarios"),
-          BottomNavigationBarItem(icon: Icon(Icons.check_circle_outline_sharp), label: "Por Hacer"),
-          BottomNavigationBarItem(icon: Icon(Icons.local_atm_sharp), label: "Recompenzas"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today_sharp), label: "Diarios"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle_outline_sharp), label: "Por Hacer"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.local_atm_sharp), label: "Recompenzas"),
         ],
         currentIndex: _currentPage,
         unselectedItemColor: Colors.black54,
@@ -63,15 +100,16 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  pageChanged(int page){
-    setState((){
+  pageChanged(int page) {
+    setState(() {
       _currentPage = page;
     });
   }
 
-  pageChanger(int page){
-    setState((){
-      _pageController.animateToPage(page, duration: Duration(milliseconds: 300), curve: Curves.ease);
+  pageChanger(int page) {
+    setState(() {
+      _pageController.animateToPage(page,
+          duration: Duration(milliseconds: 300), curve: Curves.ease);
       _currentPage = page;
     });
   }
