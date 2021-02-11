e81822e13e9cc0e19929d6f809ad730c2edfd4ff
PAOLO BUFALINO MORA <21-e7110311@users.noreply.gitlab.tjrr.jus.br>
2021-02-11 16:03:51 -0400

    custom ListTile with shadow and clipping, pending: standardize attributes.

--- lib/home_screen/widgets/task_card.dart
+++ lib/home_screen/widgets/task_card.dart
@@ -1,121 +1,124 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white70,
+    return Container(
+      padding: EdgeInsets.all(5),
       child: IntrinsicHeight(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.blue,
-              child: Material(
-                color: Colors.transparent,
-                elevation: 1,
-                child: InkWell(
-                  onTap: () {
-                    debugPrint("add");
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Icon(Icons.add, color: Colors.white),
-                        Icon(
-                          Icons.add_circle,
-                          color: Colors.blueGrey,
-                        ),
-                      ],
+        child: Container(
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.9),
+                spreadRadius: 1,
+                blurRadius: 1,
+                offset: Offset(0, 1), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.blue,
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      debugPrint("add");
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Icon(Icons.add, color: Colors.white),
+                          Icon(
+                            Icons.add_circle,
+                            color: Colors.blueGrey,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'task title',
-                    style: TextStyle(
-                        color: Colors.black87,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16),
-                    overflow: TextOverflow.fade,
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white70,
                   ),
-                  Text(
-                    'Task description',
-                    style: TextStyle(color: Colors.black54),
-                    overflow: TextOverflow.fade,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              child: Material(
-                color: Colors.transparent,
-                elevation: 1,
-                child: InkWell(
-                  onTap: () {
-                    debugPrint("remove");
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Icon(Icons.remove, color: Colors.white),
-                        Icon(
-                          Icons.remove_circle,
-                          color: Colors.blueGrey,
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {
+                        debugPrint("card");
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'task title',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                              overflow: TextOverflow.fade,
+                            ),
+                            Text(
+                              'Task description',
+                              style: TextStyle(color: Colors.black54),
+                              overflow: TextOverflow.fade,
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Container(
+                color: Colors.blue,
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      debugPrint("remove");
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Icon(Icons.remove, color: Colors.white),
+                          Icon(
+                            Icons.remove_circle,
+                            color: Colors.blueGrey,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-/*
-
-Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: MaterialButton(color: Colors.blue, child: Icon(Icons.add_circle), onPressed: (){
-                debugPrint("olis");
-              },),
-              title: Text('task title'),
-              subtitle: Text('Task description'),
-              trailing: Icon(Icons.remove_circle),
-            ),
-          ],
-        ),
-      ),
-    );
-
-Container(
-                color: Colors.blue,
-                child: Stack(
-                  children: [
-                    Icon(Icons.remove, color: Colors.white),
-                    Icon(Icons.remove_circle, color: Colors.blueGrey,),
-                  ],
-                ),
-              ),
- */
