a03ca4ad4f735a7afa0f642973d18b77a2dddc0e
PAOLO BUFALINO MORA <21-e7110311@users.noreply.gitlab.tjrr.jus.br>
2021-02-11 12:14:25 -0400

    add custom ListTile, pending: improve clipping border.

--- lib/home_screen/widgets/task_card.dart
+++ lib/home_screen/widgets/task_card.dart
@@ -3,16 +3,84 @@ import 'package:flutter/material.dart';
 class TaskCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.add_circle),
-              title: Text('task title'),
-              subtitle: Text('Task description'),
-              trailing: Icon(Icons.remove_circle),
+    return Card(
+      color: Colors.white70,
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.blue,
+              child: Material(
+                color: Colors.transparent,
+                elevation: 1,
+                child: InkWell(
+                  onTap: () {
+                    debugPrint("add");
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Icon(Icons.add, color: Colors.white),
+                        Icon(
+                          Icons.add_circle,
+                          color: Colors.blueGrey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'task title',
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                    overflow: TextOverflow.fade,
+                  ),
+                  Text(
+                    'Task description',
+                    style: TextStyle(color: Colors.black54),
+                    overflow: TextOverflow.fade,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              child: Material(
+                color: Colors.transparent,
+                elevation: 1,
+                child: InkWell(
+                  onTap: () {
+                    debugPrint("remove");
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Icon(Icons.remove, color: Colors.white),
+                        Icon(
+                          Icons.remove_circle,
+                          color: Colors.blueGrey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
@@ -22,6 +90,24 @@ class TaskCard extends StatelessWidget {
 }
 
 /*
+
+Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: MaterialButton(color: Colors.blue, child: Icon(Icons.add_circle), onPressed: (){
+                debugPrint("olis");
+              },),
+              title: Text('task title'),
+              subtitle: Text('Task description'),
+              trailing: Icon(Icons.remove_circle),
+            ),
+          ],
+        ),
+      ),
+    );
 
 Container(
                 color: Colors.blue,
